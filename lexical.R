4ae5eb7cfc5fa341f33c15710131cf23fba5415d
Sakina Zaveri <sakinazaveri0609@gmail.com>
2020-05-09 22:12:18 +0530

    Lexical-Scoping Program

---
+++ lexical.R
@@ -0,0 +1,28 @@
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inv) m <<- inv
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
+
+s <- makeCacheMatrix(matrix(1:4,2,2))
+cacheSolve(s)
